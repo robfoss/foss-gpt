30c0ab1dea7e5f1e897f15580ecdae4a6eeeffc9
robfoss <robfoss@rocketmail.com>
2023-05-26 16:04:24 -0300

    added auth sign

--- src/app/chat/[[...chatId]]/page.js
+++ src/app/chat/[[...chatId]]/page.js
@@ -1,6 +1,16 @@
+'use client';
 import React from 'react';
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 
 const ChatIDPage = () => {
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/api/auth/signin*/chat');
+    },
+  });
+
   return <div>ChatPage</div>;
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,36 @@
-import Image from 'next/image';
+'use client';
 import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  const user = session?.user;
+
+  if (status === 'loading') return <div>Loading...</div>;
+
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href='/api/auth/signin'>Log in</Link>
-    </div>
+    <>
+      <div className='flex justify-center items-center min-h-screen w-full bg-janes_black_light text-janes_white text-center'>
+        <div>
+          {!!user && <Link href='/api/auth/signout'>Sign Out</Link>}
+          {!user && (
+            <>
+              <Link
+                href='/api/auth/signin'
+                className='rounded-md bg-[#48A14D]  px-4 py-2 text-janes_white hover:bg-janes_green'
+              >
+                Sign In
+              </Link>
+              <Link
+                href='/api/auth/signin'
+                className='rounded-md bg-[#48A14D]  px-4 py-2 text-janes_white hover:bg-janes_green'
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
